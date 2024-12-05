554d4a0be2186014f31c541bd8de4b9a8974f303
user1 <user1@DESKTOP-5LPUTME>
2024-12-05 17:35:06 +0900

    [ADD] Day01 첫째날 eclipse 코드
    1. Hello2030, 첫 자바 실행

---
+++ src/Hello2030.java
@@ -0,0 +1,15 @@
+
+public class Hello2030 {
+	public static void main(String[] args) {
+		
+		int n= 2030;
+		double second = 11.18;
+		String name = "럼프햄";
+		char mbti = 'T';
+		boolean result = true;
+		// 리터럴로 사용할 수 있는 값들
+		// 숫자(정수, 실수), 문자(문자, 문자열), 참/거짓
+		System.out.println("헬로 "+n);
+		
+	}
+}
